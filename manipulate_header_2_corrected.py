4b1ee154c872c8ff74110bdb756b90d7d7c122f7
AnnaMarini12 <59647355+AnnaMarini12@users.noreply.github.com>
2020-01-27 16:20:52 +0100

    Add files via upload

---
+++ manipulate_header_2_corrected.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+from astropy.io import fits
+
+def update_keyword(key, valore, pattern):
+
+    for filename in pattern:
+        with fits.open(filename,'update') as hdul:
+            hdr = hdul[0].header
+            if key in hdr.keys():
+                print(filename+":"+key+" "+hdr[key]+" --> "+valore)
+            else:
+                print(filename+": "+key+" undefined --> "+valore)
+            hdr[key] = valore
+
+
+def main():
+    key = sys.argv[1]
+    valore = sys.argv[2]
+    pattern = sys.argv[3:]
+
+    update_keyword(key, valore, pattern)
+
+if __name__ == '__main__':
+    import sys
+    if len(sys.argv) < 4:
+        print("Usage:  "+sys.argv[0]+"KEYWORD value <list of FITS files>")
+        sys.exit()
+
+    main()
