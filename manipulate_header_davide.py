a2916ef2bb5ce26a0283834fa3ebd73170ddf5ea
Davide Ricci <davide.ricci@oapd.inaf.it>
2020-02-12 18:45:53 +0100

    Modified funtion keyword and method names to make it work

--- manipulate_header_davide.py
+++ manipulate_header_davide.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-import sys
 from astropy.io import fits
 
 def update_keyword(key, valore, pattern):
@@ -12,7 +11,7 @@ def update_keyword(key, valore, pattern):
     for filename in pattern:  # Così non c'è bisogno di glob! Vedi (b).
         with fits.open(filename,'update') as hdulist:
             hdr = hdulist[0].header
-            if key in hdr.keys(): # hdr ha un sacco di metodi, vedi (c).
+            if key.upper() in hdr.keys(): # hdr ha un sacco di metodi, vedi (c).
                 print(filename+": "+key+" "+hdr[key]+" --> "+valore)
             else:
                 print(filename+": "+key+" undefined --> "+valore)
@@ -20,13 +19,15 @@ def update_keyword(key, valore, pattern):
 
 
 def main():        
-    key = sys.argv[1].upper() # Keyword in maiuscolo. Vedi (a).
+    key = sys.argv[1] # Keyword in maiuscolo. Vedi (a).
     valore = sys.argv[2]      # Valore
     pattern = sys.argv[3:]    # File(s). "3:" significa "dal 3 in poi".
 
     update_keyword(key, valore, pattern)
 
+    
 if __name__ == '__main__':
+    import sys
 
     if len(sys.argv) < 4 :    # C'è anche lo [0] che è il nome del file :)
         print("Usage:  "+sys.argv[0]+" KEYWORD value <list of FITS files>")
--- matching_catalog1.py
+++ matching_catalog1.py
@@ -7,9 +7,9 @@ from astropy.wcs import WCS
 def get_wcs(pattern):
     for filename in pattern:
         with fits.open(filename, 'update') as hdul:
-            hdr = hdul[1].header
+            hdr = hdul[0].header
             key = hdr['OBJECT']
-            if key == 'GJ3470':
+            if key == 'wasp16':
                 w = WCS(filename)
                 print(filename)
                 print(key)
@@ -17,7 +17,7 @@ def get_wcs(pattern):
 
                 image_data = fits.getdata(filename, ext=0)
                 from photutils import detect_threshold
-                threshold = detect_threshold(image_data, nsigma=2.)
+                threshold = detect_threshold(image_data, 2)
 
                 from astropy.convolution import Gaussian2DKernel
                 from astropy.stats import gaussian_fwhm_to_sigma
@@ -35,7 +35,7 @@ def get_wcs(pattern):
                 fig, (ax1, ax2) = plt.subplots(2, 1, figsize=(10, 12.5))
                 ax1.imshow(image_data, origin='lower', cmap='Greys_r', norm=norm)
                 ax1.set_title('Data')
-                cmap = segm.make_cmap(random_state=12345)
+                cmap = segm.cmap(random_state=12345)
                 ax2.imshow(segm, origin='lower', cmap=cmap)
                 ax2.set_title('Segmentation Image')
                 plt.show()
