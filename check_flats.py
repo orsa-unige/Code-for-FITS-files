d48be6cd51b40b5d4990d57d653d6d5cd7e48d57
AnnaMarini12 <59647355+AnnaMarini12@users.noreply.github.com>
2020-06-15 01:28:43 +0200

    Update check_flats.py

--- check_flats.py
+++ check_flats.py
@@ -5,22 +5,29 @@ import numpy as np
 import matplotlib.pyplot as plt
 import warnings
 
-def detect_flats(data):
+def detect_flats(data, size, saturated = 65536, min_val = 10000, max_val = 55000):
+    lenr, lenc = int(data.shape[0]/size), int(data.shape[1]/size)
     NBINS = 1000
     histogram = plt.hist(data.flatten(), NBINS)
-    data_split = np.array_split(data, 100)
+    data_split = np.array([data[i*size:(i+1)*size,j*size:(j+1)*size]
+          for (i,j) in np.ndindex(lenr,lenc)]).reshape(lenr,lenc,size,size)
     data_split_avg = [np.mean(arr) for arr in data_split]
-    for n in data_split_avg: 
-        goodflat_condition = 10000<n<55000
+    for avg in data_split_avg:
+        goodflat_condition = min_val<avg<max_val
         if goodflat_condition == True:
             is_it_a_good_flat = True
-        elif n >= 65536:
+            print(is_it_a_good_flat)
+            continue
+        elif avg >= saturated:
             is_it_a_good_flat = False
-            warnings.warn('SATURATED FLAT ' + filename)
+            print(is_it_a_good_flat)
+            warnings.warn('SATURATED FLAT ')
+            break
         else:
-             is_it_a_good_flat = False
-             warnings.warn('NONLINEAR FLAT REGIME ' + filename)
+            is_it_a_good_flat = False
+            print(is_it_a_good_flat)
+            warnings.warn('NONLINEAR FLAT REGIME ')
+            break
 ##    plt.show()
     return(is_it_a_good_flat)
         
-                   
